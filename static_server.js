2f5267680ccc668c29223f34a580d8b302703d95
James Dunne <jdunne@kaplan.edu>
2015-03-03 19:10:15 -0600

    Adding static file server for node.js for dev purposes.

---
+++ static_server.js
@@ -0,0 +1,52 @@
+var http = require("http"),
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs"),
+    qs = require("querystring"),
+    port = process.argv[2] || 8888;
+
+http.createServer(function(request, response) {
+
+  var uri = qs.unescape(url.parse(request.url).pathname)
+    , filename = path.join(process.cwd(), uri);
+  var tmp = uri.lastIndexOf(".");
+  var extension = null;
+  if (tmp >= 0) extension = uri.substring((tmp + 1));
+
+  console.log(filename);
+  fs.exists(filename, function(exists) {
+    if(!exists) {
+      response.writeHead(404, {"Content-Type": "text/plain"});
+      response.write("404 Not Found\n");
+      response.end();
+      return;
+    }
+
+    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+
+    fs.readFile(filename, "binary", function(err, file) {
+      if(err) {
+        response.writeHead(500, {"Content-Type": "text/plain"});
+        response.write(err + "\n");
+        response.end();
+        return;
+      }
+
+      if (extension === null || extension === 'html') response.writeHead(200, {"Content-Type": 'text/html'});
+      else if (extension === 'htm') response.writeHead(200, {"Content-Type": 'text/html'});
+      else if (extension === 'css') response.writeHead(200, {"Content-Type": 'text/css'});
+      else if (extension === 'js') response.writeHead(200, {"Content-Type": 'text/javascript'});
+      else if (extension === 'png') response.writeHead(200, {"Content-Type": 'image/png'});
+      else if (extension === 'jpg') response.writeHead(200, {"Content-Type": 'image/jpeg'});
+      else if (extension === 'jpeg') response.writeHead(200, {"Content-Type": 'image/jpeg'});
+      else if (extension === 'wav') response.writeHead(200, {"Content-Type": 'audio/wav'});
+      else response.writeHead(200, {"Content-Type": 'application/octet-stream'});
+
+      response.write(file, "binary");
+      response.end();
+      //response.end(file);
+    });
+  });
+}).listen(parseInt(port, 10));
+
+console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
